ca594a0487eea504b3f944d76e3faf4b32d4f898
clieantapp-crypto <clieantapp@gmail.com>
2025-11-13 03:32:59 +0300

    ssss

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,7 @@
 
 
 export const playNotificationSound = () => {
-    const audio=new Audio('/ding-126626.mp3')
+    const audio=new Audio('/long-expected-548.mp3')
     if (audio) {
       audio!.play().catch((error) => {
         console.error('Failed to play sound:', error);
